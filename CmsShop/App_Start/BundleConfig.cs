9a4e77f0e70b04cd8a80ce3d88f573eba8468919
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2018-12-01 17:26:05 +0100

    Dashboard Amin

--- CmsShop/App_Start/BundleConfig.cs
+++ CmsShop/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace CmsShop
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                       "~/Scripts/jquery-{version}.js",
+                       "~/Scripts/bootstrap.js"));
         }
     }
 }
