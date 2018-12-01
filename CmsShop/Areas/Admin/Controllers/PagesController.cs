5a5621475e8c3798761ce1023966d57d096a8c35
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2018-12-01 18:14:03 +0100

    First row in Data Base

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,19 @@ namespace CmsShop.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            // deklaracja listy PageVM
+            List<PageVM> pagesList;
+
+      
+
+            using (Db db = new Db())
+            {
+                // inicializacja listy
+                pagesList = db.Pages.ToArray().OrderBy(x=>x.Sorting).Select(x=> new PageVM(x)).ToList();
+            }
+
+            //zwracamy strony do widoku 
+            return View(pagesList);
         }
     }
 }
