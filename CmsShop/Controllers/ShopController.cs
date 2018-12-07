aeb3a2a799c8b8fd9d571353d0a0842fa5acf32d
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2018-12-07 13:19:00 +0100

    Front Shop Showed Details Product

--- CmsShop/Controllers/ShopController.cs
+++ CmsShop/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using CmsShop.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,7 +32,7 @@ namespace CmsShop.Controllers
             // zwramy partial z lista 
             return PartialView(categoryVMList);
         }
-
+        //GET: / shop/Category
         public ActionResult Category(string name)
         {
             // deklaracia productVMList
@@ -62,6 +63,36 @@ namespace CmsShop.Controllers
             }
             //zwracamy widok z listą produktów z danej kategorii 
             return View(productVMList);
+        }
+        //GET: / shop/product-szczegoly/name
+        [ActionName("product-szczegol")]
+        public ActionResult ProductDetails(string name)
+        {
+            // deklaracja product VM i product dto
+            ProductVM model;
+            ProductDTO dto;
+            // inicjalizacja product id
+            int id = 0;
+            using (Db db = new Db())
+            {// sprazamy czy product istniej
+
+                if (!db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index" ,"Shop");
+                }
+                // inicjalizacja productDTO
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+                // pobranie=id
+                id = dto.Id;
+                // inicjalizacja modelu 
+                model = new ProductVM(dto);
+            }
+
+            // pobrać gallery obrazków dla wybrango produktu
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                .Select(fn => Path.GetFileName(fn));
+            //zwracamy wdok z modelem 
+            return View("ProductDetails",model);
         }
 
 
