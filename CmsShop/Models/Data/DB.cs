77b6aba85d28933cf226b7cb8115b7b1dc98a6bf
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2018-12-10 19:37:20 +0100

    Tabele Użytkowników

--- CmsShop/Models/Data/DB.cs
+++ CmsShop/Models/Data/DB.cs
@@ -13,6 +13,9 @@ namespace CmsShop.Models.Data
         public DbSet<CategoryDTO> Categories { get; set; }
         public DbSet<ProductDTO> Products { get; set; }
     
+        public DbSet<UserDTO> Users { get; set; }
+
+        public DbSet<RoleDTO> Roles { get; set; }
 
     }
 }
