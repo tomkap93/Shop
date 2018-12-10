a304769d1b766c7f8f6a4048df735edbda3d2545
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2018-12-10 13:51:01 +0100

    Cart part 1

---
+++ CmsShop/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using CmsShop.Models.ViewModels.Cart;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CmsShop.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            var cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+            if (cart.Count==0||Session["cart"]==null)
+            {
+                ViewBag.Message = "Twój koszyk jest pusty";
+                return View();
+            }
+
+            // Obliczenie wartości podusuwującej koszyka 
+            // i przekazanie do viewbag
+            decimal total = 0M;
+            foreach (var item in cart)
+            {
+                total += item.Total;
+            }
+            ViewBag.GrandTotal = total;
+            return View(cart);
+        }
+
+        public ActionResult CartPartial()
+        {
+            //inicjalizacja cart VM
+            CartVM model = new CartVM();
+            // inicjalizacja ilość i cena
+            int qty = 0;
+            decimal price = 0M;
+
+            //sprawdzamy czy mamy dane koszyka zapisane w sesi
+            if (Session["cart"]!=null)
+            {
+                //pobieranie wartości z sesji 
+                var list = (List<CartVM>)Session["cart"];
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity*item.Price;
+                }
+            }
+            else
+            {
+                // ustawiamy ilość i cena na 0 
+                qty = 0;
+                price = 0M;
+            }
+
+
+            return PartialView(model);
+        }
+    }
+}
