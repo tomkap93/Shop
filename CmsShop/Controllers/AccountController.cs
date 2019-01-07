d97bf816a3a94b5159d42eeff48211822dbd35f4
Tomasz Kapichke <tomasz.kapischke@gmail.com>
2019-01-07 21:06:03 +0100

    Rejestracja i logowanie

--- CmsShop/Controllers/AccountController.cs
+++ CmsShop/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -32,6 +34,70 @@ namespace CmsShop.Controllers
             // zwracamy widok
             return View();
         }
+
+        [HttpPost]
+          [ActionName("create-account")]
+   public ActionResult CreateAccount(UserVM model)
+        {
+            // sprawdzenie model state
+            if(!ModelState.IsValid)
+            {
+                return View("CreateAccount",model);
+            }
+            // sprawdzenie hasła 
+            if(!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("","Hasła nie są identyczne");
+                return View("CreateAccount", model);
+            }
+      
+            using (Db db = new Db())
+            {
+                //sprawdzanie czy nazwa użytkownika jest unikalna 
+                if (db.Users.Any(x=> x.UserName.Equals(model.UserName)))
+                {
+                    ModelState.AddModelError("", "Nazwa użytkownika "+model.UserName+" jest już zajęta");
+                    model.UserName = "";
+                    return View("CreateAccount", model);
+                }
+                // utworzenie użytkownika
+                UserDTO dto = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    UserName = model.UserName,
+                    Password = model.Password,
+
+
+                };
+                // dodanie użytkownika do bazy
+                db.Users.Add(dto);
+                // zapis na bazie
+                db.SaveChanges();
+
+                // dodanie roli 
+                UserRoleDTO userRoleDTO = new UserRoleDTO() {
+                    UserId = dto.Id,
+                    RoleId = 2
+                };
+                // ustawienie roli
+                db.UserRoles.Add(userRoleDTO);
+                // dodanie do bazy
+                db.SaveChanges();
+
+            }
+
+            // tempdate komunikat
+            TempData["SM"] = "Jesteś teraz zarejestrowwany i możesz się zalogować!!";
+        
+
+
+
+            return Redirect("~/account/login");
+        }
+
+
 
     }
 }
